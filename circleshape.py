cc9bf9a81745815ac8160dd46fa01e2f0ff3e4c1
yeenbean <yeenimal@pm.me>
2024-09-03 18:41:34 -0700

    Add additional instance vars for circles

--- circleshape.py
+++ circleshape.py
@@ -6,6 +6,10 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__(self.containers)
         else:
             super().__init__()
+        
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
     
     def draw(self, screen):
         pass
