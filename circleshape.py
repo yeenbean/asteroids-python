66b4505859bedad19974604db0318230ba85ad88
yeenbean <yeenimal@pm.me>
2024-09-03 17:14:33 -0700

    Create skeleton module for circle-based sprites

---
+++ circleshape.py
@@ -0,0 +1,14 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+    
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
