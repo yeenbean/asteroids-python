8db9d8c71cbd2682738979b41f6e3cb81dd3ca21
yeenbean <yeenimal@pm.me>
2024-09-03 17:09:37 -0700

    Create game clock and implement tick rate

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 
 screen = None
+clock  = None
+dt     = None
 
 def main():
     print("Starting asteroids!")
@@ -10,8 +12,12 @@ def main():
 
     pygame_init = pygame.init()
     print(f"pygame loaded {pygame_init[0]} out of {pygame_init[0] + pygame_init[1]} modules successfully.")
+
+    clock = pygame.time.Clock()
+    dt = 0
+    print("Game clock initialized")
     
-    print("Creating game screen")
+    print("Creating game screen...")
     try:
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         print("Screen created successfully")
@@ -19,6 +25,7 @@ def main():
         print(f"Error creating screen: {e}")
     
     while True:
+        # pre
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -29,6 +36,9 @@ def main():
 
         # Draw
         screen.fill(SCREEN_BACKGROUND_COLOR)
+
+        # post
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
