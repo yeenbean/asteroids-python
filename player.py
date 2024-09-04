14fdab6b200bc7466cc89e5715e93fc1aa774a86
yeenbean <yeenimal@pm.me>
2024-09-03 22:05:47 -0700

    Add input handling for player sprite

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import *
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -18,3 +18,13 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def input(self, dt, keys):
+        if keys[pygame.K_a]:
+            self.rotation += PLAYER_TURN_SPEED * (dt * -1)
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.position += (
+                pygame.Vector2(0, 1).rotate(self.rotation)
+            ) * PLAYER_SPEED * dt
